ccf70093304599624501c9c6b15cfde6f950719b
cobweb <codename.cobweb@gmail.com>
2018-04-03 18:05:18 +0300

    first commit

---
+++ server/app_api/controllers/main.js
@@ -0,0 +1,31 @@
+var Vacation = require('../models/vacation');
+
+var sendJSONresponse = function (res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+module.exports.index = function (req, res) {
+    sendJSONresponse(res, 200, {
+        status: 'success'
+    });
+};
+
+module.exports.vacations = function (req, res) {
+    Vacation.find({
+        available: true
+    }, function (err, vacations) {
+        var context = {
+            vacations: vacations.map(function (vacation) {
+                return {
+                    sku: vacation.sku,
+                    name: vacation.name,
+                    description: vacation.description,
+                    price: vacation.getDisplayPrice(),
+                    inSeason: vacation.inSeason,
+                }
+            })
+        };
+        sendJSONresponse(res, 200, context);
+    });
+};
